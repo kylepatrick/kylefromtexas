014fcb4e272ddec3b84c896c63d2cc53e93d9e19
kylepatrick <kyle@kylepatrick.com>
2025-07-11 12:35:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './kft.png';
+import tiktoklogo from "./TikTok_logo.svg.png";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   <table><tr><td>  <img src={logo} className="App-logo" alt="logo" /></td><td></td></tr>
+   
+
+<tr>
+  <td>
+    <br></br><br></br><br></br><br></br><br></br>
+  </td>
+  <td></td>
+</tr>
+<tr>
+  <td>
+  <a target="_blank" href="https://www.tiktok.com/@404kylepatrick"><img height="100px" src={tiktoklogo}></img></a>
+  </td>
+  <td></td>
+</tr>
+ <tr>
+  <td>
+    Official T-Shirt<br></br>
+    <a target="_blank" href="https://www.amazon.com/Kyle-From-Texas-Official-Supporter/dp/B0FH5ZWJWW/ref=sr_1_1?dib=eyJ2IjoiMSJ9.H9fcrDPh5LUxXSUOPRTx-X8ZeaJ3GR_SS7xcRxZym8LGjHj071QN20LucGBJIEps.T5XVcthN-ibK2_0Q39H7x39HCtQiODvTGe91-OhZ1b8&dib_tag=se&qid=1752251850&refinements=p_4%3AKyle%2BFrom%2BTexas&s=apparel&sr=1-1&customId=B0752XS9NL&customizationToken=MC_Assembly_1%23B0752XS9NL&th=1&psc=1"><img height="400px" src="https://m.media-amazon.com/images/I/A1NbHftLLnL._CLa%7C2140%2C2000%7C51QbgCL19VL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SX679_.png"></img></a>
+  </td>
+  <td></td>
+</tr>
+</table>
+       
+ </header>
+    </div>
+  );
+}
+
+export default App;
